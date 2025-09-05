8ed777a66e3b32b336b9b66e931b5c3591610ad6
Kirill <kirillponomarev147@gmail.com>
2025-09-05 15:52:04 +0300

    Create new entities

---
+++ DirectoryService/src/DirectoryService.Domain/Shared/Timezone.cs
@@ -0,0 +1,29 @@
+﻿using CSharpFunctionalExtensions;
+using TimeZoneConverter;
+
+namespace DirectoryService.Domain.Shared;
+
+public record Timezone
+{
+    private Timezone(string value, TimeZoneInfo timeZoneInfo)
+    {
+        Value = value;
+        TimeZoneInfo = timeZoneInfo;
+    }
+
+    public string Value { get; }
+    public TimeZoneInfo TimeZoneInfo { get; }
+
+    public static Result<Timezone, string> Create(string value)
+    {
+        if (TZConvert.TryGetTimeZoneInfo(value, out var timezone))
+        {
+            return new Timezone(value, timezone);
+        }
+        else
+        {
+            var zones = TZConvert.KnownIanaTimeZoneNames;
+            return "Invalid timezone. Available zones:\n" + string.Join("\n", zones);
+        }
+    }
+}
